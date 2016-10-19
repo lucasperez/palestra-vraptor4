bb1193852b83c1b6f21b5068b185954ea9f0455e
Lucas Perez <lucascperez@gmail.com>
2016-10-19 21:14:30 -0200

    ping pra build do codeship

--- src/test/java/br/com/palestra/controller/PrimeiroControllerTest.java
+++ src/test/java/br/com/palestra/controller/PrimeiroControllerTest.java
@@ -3,20 +3,19 @@ package br.com.palestra.controller;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import br.com.caelum.vraptor.util.test.MockResult;
 
 public class PrimeiroControllerTest {
-	
+
 	private MockResult result;
-    private PrimeiroController controller;
-	
-    @Before
-    public void setUp() {
-        result = new MockResult();
-        controller = new PrimeiroController(result);
-    }
-    
+	private PrimeiroController controller;
+
+	@Before
+	public void setUp() {
+		result = new MockResult();
+		controller = new PrimeiroController(result);
+	}
+
 	@Test
 	public void testHome() {
 		controller.home();
